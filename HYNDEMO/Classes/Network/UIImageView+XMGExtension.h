5bbc22d4ca408f023b6545978a78b22b58e6b405
jie.huang <jie.huang@jiehuangdeMacBook-Pro.local>
2018-04-02 17:28:52 +0800

    ..

--- HYNDEMO/Classes/Network/UIImageView+XMGExtension.h
+++ HYNDEMO/Classes/Network/UIImageView+XMGExtension.h
@@ -1,18 +1,19 @@
+////
+////  UIImageView+XMGExtension.h
+////  百思不得姐
+////
+////  Created by 王顺子 on 16/6/16.
+////  Copyright © 2016年 小码哥. All rights reserved.
+////
 //
-//  UIImageView+XMGExtension.h
-//  百思不得姐
+//#import <UIKit/UIKit.h>
+//#import "UIImage+XMGImage.h"
 //
-//  Created by 王顺子 on 16/6/16.
-//  Copyright © 2016年 小码哥. All rights reserved.
+//@interface UIImageView (XMGExtension)
 //
+//- (void)setURLImageWithURL: (NSURL *)url progress:(void(^)(CGFloat progress))progress complete: (void(^)(void))complete;
+//
+//- (void)setURLImageWithURL: (NSURL *)url placeHoldImage:(UIImage *)placeHoldImage isCircle:(BOOL)isCircle;
+//
+//@end
 
-#import <UIKit/UIKit.h>
-#import "UIImage+XMGImage.h"
-
-@interface UIImageView (XMGExtension)
-
-- (void)setURLImageWithURL: (NSURL *)url progress:(void(^)(CGFloat progress))progress complete: (void(^)(void))complete;
-
-- (void)setURLImageWithURL: (NSURL *)url placeHoldImage:(UIImage *)placeHoldImage isCircle:(BOOL)isCircle;
-
-@end
