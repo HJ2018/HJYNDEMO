132d6df5a67f96e888754f5eb51865e190decf04
jie.huang <jie.huang@jiehuangdeMacBook-Pro.local>
2018-04-02 15:13:39 +0800

    ..

--- Example/Pods/Target Support Files/Pods-HYNDEMO_Example/Pods-HYNDEMO_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-HYNDEMO_Example/Pods-HYNDEMO_Example-frameworks.sh
@@ -134,10 +134,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HYNDEMO/HYNDEMO.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HYNDEMO/HYNDEMO.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- HYNDEMO.podspec
+++ HYNDEMO.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'HYNDEMO'
-  s.version          = '1.1.2'
+  s.version          = '1.1.3'
   s.summary          = 'HYNDEMO.'
 
 # This description is used to generate tags and improve search results.
@@ -38,5 +38,6 @@ HYNDEMO.分类使用很方便
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+    s.dependency 'AFNetworking'
+    s.dependency 'SDWebImage'
 end
